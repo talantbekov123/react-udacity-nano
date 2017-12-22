acc1e10d48f09c39c29c29f79519ff902ea32661
Kairat <talantbekov123@gmail.com>
2017-12-22 18:15:23 +0600

    Add state updates

--- contacts-my-react/src/App.js
+++ contacts-my-react/src/App.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react'
 import ListContacts from './listContacts.js'
 
-var contacts = [
-	{
-		id: 'ryan',
-		name: 'Ryan Florence',
-		email: 'ryan@reacttraining.com',
-		avatarURL: '../public/ryan.jpg'
-	},
-	{
-		id: 'michael',
-		name: 'Michael Jackson',
-		email: 'michael@reacttraining.com',
-		avatarURL: '../public//michael.jpg'
-	},
-	{
-		id: 'tyler',
-		name: 'Tyler McGinnis',
-		email: 'tyler@reacttraining.com',
-		avatarURL: '../public/tyler.jpg'
-	}
-]
-	
 class App extends React.Component {
+	state = {
+		contacts: [
+			{
+				id: 'ryan',
+				name: 'Ryan Florence',
+				email: 'ryan@reacttraining.com',
+				avatarURL: '../public/ryan.jpg'
+			},
+			{
+				id: 'michael',
+				name: 'Michael Jackson',
+				email: 'michael@reacttraining.com',
+				avatarURL: '../public//michael.jpg'
+			},
+			{
+				id: 'tyler',
+				name: 'Tyler McGinnis',
+				email: 'tyler@reacttraining.com',
+				avatarURL: '../public/tyler.jpg'
+			}
+		]
+	}
+
+	removeContact = (contact) => {
+    	this.setState((state) => ({
+      		contacts: state.contacts.filter((c) => c.id !== contact.id)
+    	}))
+  	}
+
 	render() {
 		return(
 			<div>
-				<ListContacts contacts={contacts}/>
+				<ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
 			</div>
 		)
 	}
--- contacts-my-react/src/listContacts.js
+++ contacts-my-react/src/listContacts.js
@@ -3,11 +3,13 @@ import React, { Components } from 'react'
 class ListComponents extends React.Component {
 	render() {
 		return (
-
 			<ol>
 				{this.props.contacts.map((contact) => (
 					<li key={contact.id}>
 						{contact.name}
+						<button onClick={() => this.props.onDeleteContact(contact)}>
+							remove
+						</button>
 					</li>
 				))}
 			</ol>
